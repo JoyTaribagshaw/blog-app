3170f50798a7bbf2d8fa68dc80863ddeb5cc5180
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-10-26 19:03:53 +0000

    fix linter error

--- db/migrate/20231026163527_create_user.rb
+++ db/migrate/20231026163527_create_user.rb
@@ -1,10 +1,10 @@
 class CreateUser < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-t.string :name
-t.string :photo
-t.string :bio  
-t.integer :post_counter
+      t.string :name
+      t.string :photo
+      t.string :bio
+      t.integer :post_counter
       t.timestamps
     end
   end
