74e13bec1150b9dba84c260e63f05bf7632bccbe
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-10-26 19:23:24 +0000

    update user table

--- db/migrate/20231026163527_create_user.rb
+++ db/migrate/20231026163527_create_user.rb
@@ -2,8 +2,8 @@ class CreateUser < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
-      t.string :bio
+      t.text :photo
+      t.text :bio
       t.integer :post_counter
       t.timestamps
     end
