5ed97df94fd35cdafebf7292ac77fb2fe860265c
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-10-26 17:21:58 +0000

    create data model for post

---
+++ db/migrate/20231026163852_create_post.rb
@@ -0,0 +1,12 @@
+class CreatePost < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.references :author, foreign_key: { to_table: :users }
+      t.string :title
+      t.text :text
+      t.integer :likes_counter
+      t.integer :comments_counter
+      t.timestamps
+    end
+  end
+end
