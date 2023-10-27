1cf609cf317f5237809739f42feb6b34810392a6
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-10-27 19:20:02 +0000

    create model classes for comment

---
+++ db/migrate/20231027182636_create_comment.rb
@@ -0,0 +1,10 @@
+class CreateComment < ActiveRecord::Migration[7.1]
+  def change
+    create_table :comments do |t|
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
+      t.text :text
+      t.timestamps
+    end
+  end
+end
