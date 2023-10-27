67d4446937f3ff06c3bbce6a26d4c4011d74906c
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-10-27 19:25:31 +0000

    fix linter errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    has_many :comments
-    has_many :likes
-  
-    validates :author_id, presence: true
-    validates :title, presence: true
-    validates :text, presence: true
-  
-    after_save :update_post_counter
-  
-    private
-  
-    def update_post_counter
-      update(posts_counter: posts.count)
-    end
+  has_many :comments
+  has_many :likes
+
+  validates :author_id, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    update(posts_counter: posts.count)
   end
-  
+end
