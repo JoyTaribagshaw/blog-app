b0a557d82f757295f7a92ff789f144e781160816
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-10-28 11:29:16 +0000

    fix linter errors

--- db/migrate/20231028105507_create_comments.rb
+++ db/migrate/20231028105507_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
       t.string :text
-  
+
       t.timestamps
     end
   end
--- db/migrate/20231028111220_add_user_ref_to_post.rb
+++ db/migrate/20231028111220_add_user_ref_to_post.rb
@@ -1,5 +1,5 @@
 class AddUserRefToPost < ActiveRecord::Migration[7.1]
   def change
-    add_reference :posts, :author, null: false, foreign_key: {to_table: :users}
+    add_reference :posts, :author, null: false, foreign_key: { to_table: :users }
   end
 end
