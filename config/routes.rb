98510b1b0325c93eb10486b8b5010d398789c123
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-04 18:09:36 +0000

    update rooutes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root "users#index"  
-  resources :users, only: [:index, :show] do    
-  resources :posts, only: [:index, :show]  end
+  root "users#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
+  end
 end
