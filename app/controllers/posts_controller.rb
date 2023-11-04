f8c837ae962dd31f1d70845e6a71e23bd0cce34d
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-04 17:26:56 +0000

    create forms for posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
-    @posts = @posts.paginate(page: params[:page], per_page: 2)
+    @posts = @posts.paginate(page: params[:page], per_page: 10)
   end
 
   def show
