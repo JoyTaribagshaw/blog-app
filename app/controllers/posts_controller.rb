0f4787717bdb6b7710295499bcd32906896850c6
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-08 14:44:44 +0000

    update controller files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,36 +1,28 @@
 class PostsController < ApplicationController
   layout 'standard'
-
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id]).order(id: :asc)
-    @posts = @posts.paginate(page: params[:page], per_page: 10)
-  end
-
-  # Renders the new post form
-  def new
-    @post = Post.new
-  end
-
-  # Saves a new post to the database
-  def create
-    @post = Post.new(post_params)
-    @post.author_id = current_user.id
-
-    if @post.save
-      redirect_to user_post_path(id: @post.id, user_id: current_user.id)
-    else
-      render :new
-    end
+    @posts = @posts.paginate(page: params[:page], per_page: 5)
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  private
+  def new
+    @post = Post.new
+  end
 
-  def post_params
-    params.require(:post).permit(:title, :text)
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      flash[:success] = 'Post Created!'
+      redirect_to user_posts_url
+    else
+      flash.now[:error] = 'Error: Post can not be created!'
+      render :new, locals: { post: @post }
+    end
   end
 end
