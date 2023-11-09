0e41757e7513003ef921ad6b80897608d0687b7e
JoyTaribagshaw <joytaribagshaw@gmail.com>
2023-11-09 08:19:26 +0000

    Initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Create current_user method to temporary return the first user from the database
+  Create current_user method to temporary return the first user from the database
   def current_user
     @first_user = User.first
   end
