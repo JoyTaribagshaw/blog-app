3182e5594176c9e12f007aa58edfdbbffc5569cf
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-09 11:29:05 +0300

    cleaned application controller for devise configuration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-  Create current_user method to temporary return the first user from the database
-  def current_user
-    @first_user = User.first
-  end
+  # Implementation of devise
 end
